cfecca2b2cafe9b0493a8d6186ad447fcf9eebf1
Ruslan Prnkovskij <ruslan.penkovskij@gmail.com>
2018-02-23 00:14:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/\s/g, '')
+                    .replace(/[\+]/g, ',+')
+                    .replace(/[^\t][\-]/g, ',-');
+  let parts = equation.split(',');
+  let re = new RegExp('[\-0-9]+');
+  let a = re.exec(parts[0])[0];
+  let b = re.exec(parts[1])[0];
+  let c = parts[2]
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  let solutions = [];
+  solutions[0] = Math.round((- b + Math.sqrt(discriminant)) / (2 * a));
+  solutions[1] = Math.round((- b - Math.sqrt(discriminant)) / (2 * a));
+  return solutions.sort((a, b) => a - b);
 }
